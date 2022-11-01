317d714162ed2e135aadf964455e0d3fd4985b0f
emres <emresorkun@gmail.com>
2022-11-01 15:14:57 +0100

    Add project files.

---
+++ Övning1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace Övning1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("please enter the sallary: ");
+        }
+        
+    }
+}
