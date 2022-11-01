551c09b13c6a9740c2b7ffb6fc2558ec22187d5e
Emre Sorkun <emresorkun@gmail.com>
2022-11-01 16:14:00 +0100

    answers two questions added

--- Övning1/Program.cs
+++ Övning1/Program.cs
@@ -20,8 +20,18 @@ namespace Övning1
                 sallary = Console.ReadLine();
                 Console.WriteLine($"name//{name} sallary//{sallary}");
             }
-           
 
+            //Uppgift 1 Vilka klasser bör ingå i programmet?
+
+            // here in my badly written but working programme only Program Class.
+            // If I could I should be creating a class named Employee and add two attribut to this class. Name and Salary. Or maybe two classes? I cant decide.
+
+            //Uppgift 2 Vilka attribut och metoder bör ingå i dessa klasser?
+
+            //I do not know how to create arrays populated with objects so this end up being simple. If I could do I would create an array of objects and push each employee name and sallary
+            //pair to this array. 
+
+            //Uppgift 3 Skriv programmet
 
         }
         
